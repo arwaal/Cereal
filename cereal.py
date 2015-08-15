8bb18c8bdf7cff80a125bf0b2f242486846a8ac0
Arwa <arwa@Arwas-MacBook-Pro.local>
2015-08-15 14:09:55 +0300

    cereal.py changes

--- cereal.py
+++ cereal.py
@@ -6,8 +6,8 @@ import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pro.settings")
 
-#import django
-#django.setup()
+import django
+django.setup()
 
 from maim.models import Cereal, NutriFact, Manufacturer 
 
@@ -34,29 +34,30 @@ for row in reader:
 	manufacturer_obj.name = row['Manufacturer']
 	manufacturer_obj.save()
 
+	name=row['Cereal Name']
+	if name: 
+		cereal_obj, created = Cereal.objects.get_or_create(name=row['Cereal Name'])
+		cereal_obj.name = row['Cereal Name']
+		cereal_obj.cups_per_serving = row['Cups per Serving']
+		cereal_obj.serving_size_weight = row['Serving Size Weight']
+		cereal_obj.display_shelf = row ['Display Shelf']
+		cereal_obj.t_ype = row['Type']
+		cereal_obj.manufacturer = manufacturer_obj
+		cereal_obj.save()
 
-	cereal_obj, created = Cereal.objects.get_or_create(name=row['Cereal Name'])
-	cereal_obj.name = row['Cereal Name']
-	cereal_obj.cups_per_serving = row['Cups per Serving']
-	cereal_obj.serving_size_weight = row['Serving Size Weight']
-	cereal_obj.display_shelf = row ['Display Shelf']
-	cereal_obj.t_ype = row['Type']
-	cereal_obj.manufacturer = manufacturer_obj
-	cereal_obj.save()
 
 
-
-	nutri_obj, created = NutriFact.objects.get_or_create(Cereal=cereal_obj)
-	nutri_obj.calories = row ['Calories']
-	nutri_obj.protein = row['Protein (g)']
-	nutri_obj.fat = row['Fat']
-	nutri_obj.sodium = row['Sodium']
-	nutri_obj.dietary_fiber = row['Dietary Fiber']
-	nutri_obj.carbs = row['Carbs']
-	nutri_obj.sugars = row['Sugars']
-	nutri_obj.potassium = row['Potassium']
-	nutri_obj.vitamins = row['Vitamins and Minerals']
-	nutri_obj.save()
+		nutri_obj, created = NutriFact.objects.get_or_create(Cereal=cereal_obj)
+		nutri_obj.calories = row ['Calories']
+		nutri_obj.protein = row['Protein (g)']
+		nutri_obj.fat = row['Fat']
+		nutri_obj.sodium = row['Sodium']
+		nutri_obj.dietary_fiber = row['Dietary Fiber']
+		nutri_obj.carbs = row['Carbs']
+		nutri_obj.sugars = row['Sugars']
+		nutri_obj.potassium = row['Potassium']
+		nutri_obj.vitamins = row['Vitamins and Minerals']
+		nutri_obj.save()
 
 	
 
--- pro/settings.py
+++ pro/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'Cereal', 
         'HOST': '127.0.0.1', 
         'USER': 'root', 
-        'PASSWORD': 'ana7elwa', 
+        'PASSWORD': 'root', 
         'PORT': ''
     }
 }
